82cc94f495cc8843ce3ccaba599affbd149c43d7
cheerfulbeast <mlrmramos@usep.edu.ph>
2024-10-08 17:59:21 +0800

    fix: included plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.coding.copilot-chat" },
+    { import = "plugins" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
