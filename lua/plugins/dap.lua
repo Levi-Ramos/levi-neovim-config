eaef1e99b178a08d11e136ff00db0260cb99a0cb
cheerfulbeast <mlrmramos@usep.edu.ph>
2024-10-14 15:54:17 +0800

    fix: plugins returning null

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,13 +1,19 @@
-local dap = require("dap")
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
 
-dap.configurations.dart = {
-  {
-    type = "dart",
-    request = "launch",
-    name = "zeep launch flutter (linux)",
-    flutterSdkPath = "/home/apollo/flutter/bin/flutter",
-    dartSdkPath = "/home/apollo/flutter/bin/dart",
-    program = "${workspaceFolder}/lib/main.dart",
-    cwd = "${workspaceFolder}",
-  },
+    -- add more debug adapters here
+    dap.configurations.dart = {
+      {
+        type = "dart",
+        request = "launch",
+        name = "zeep launch flutter (linux)",
+        flutterSdkPath = "/home/apollo/flutter/bin/flutter",
+        dartSdkPath = "/home/apollo/flutter/bin/dart",
+        program = "${workspaceFolder}/lib/main.dart",
+        cwd = "${workspaceFolder}",
+      },
+    }
+  end,
 }
