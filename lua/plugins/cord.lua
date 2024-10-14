cd14f1e9605647120bcffbb8ab2caa92c5dd6156
cheerfulbeast <mlrmramos@usep.edu.ph>
2024-10-14 16:22:37 +0800

    fix: moved cord configuration to opts

--- lua/plugins/cord.lua
+++ lua/plugins/cord.lua
@@ -3,24 +3,29 @@ return {
     "vyfor/cord.nvim",
     build = "./build || .\\build",
     event = "VeryLazy",
-    opts = {}, -- calls require('cord').setup()
-    buttons = {
-      {
-        label = "Github Page",
-        url = "https://github.com/Levi-Ramos",
+    opts = {
+      buttons = {
+        {
+          label = "Github Page",
+          url = "https://github.com/Levi-Ramos",
+        },
+        {
+          label = "Neovim Config",
+          url = "https://github.com/Levi-Ramos/levi-neovim-config",
+        },
       },
-    },
 
-    editor = {
-      tooltip = "Get my nvim config in my github page :D",
-    },
-
-    display = {
-      show_cursor_position = true,
-      swap_fields = true,
-      workspace_blacklist = {
-        ".env",
+      editor = {
+        tooltip = "Get my nvim config in my github page :D",
       },
-    },
+
+      display = {
+        show_cursor_position = true,
+        swap_fields = true,
+        workspace_blacklist = {
+          ".env",
+        },
+      },
+    }, -- calls require('cord').setup()
   },
 }
