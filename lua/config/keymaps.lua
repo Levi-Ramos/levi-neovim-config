3bf6a94a63df5517287719167ae850c793067443
cheerfulbeast <mlrmramos@usep.edu.ph>
2024-10-21 15:23:03 +0800

    refactor: declared cord dependency

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,43 +2,57 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- dependency variables
+local cord = require("cord")
+local flutter = require("flutter-tools")
+
 -- Discord keymaps
 vim.keymap.set("n", "<leader>Dc", function()
   print("Connecting to discord")
-  require("cord").connect()
+  cord.connect()
 end)
 
 vim.keymap.set("n", "<leader>Dr", function()
   print("Reconnecting to discord")
-  require("cord").reconnect()
+  cord.reconnect()
 end)
 
 vim.keymap.set("n", "<leader>Dd", function()
   print("Disconnect from discord")
-  require("cord").disconnect()
+  cord.disconnect()
 end)
 
 vim.keymap.set("n", "<leader>Dt", function()
   print("Toggle presence of discord")
-  require("cord").toggle_presence()
+  cord.toggle_presence()
 end)
 
 vim.keymap.set("n", "<leader>Ds", function()
   print("Show presence in discord")
-  require("cord").show_presence()
+  cord.show_presence()
 end)
 
 vim.keymap.set("n", "<leader>Dh", function()
   print("hide presence in discord")
-  require("cord").hide_presence()
+  cord.hide_presence()
 end)
 
 vim.keymap.set("n", "<leader>Di", function()
   print("Show idle status")
-  require("cord").idle()
+  cord.idle()
 end)
 
 vim.keymap.set("n", "<leader>Du", function()
   print("Unhide idle status")
-  require("cord").unidle()
+  cord.unidle()
 end)
+
+-- flutter plugin tools keymaps
+vim.keymap.set("n", "<leader>Frr", ":FlutterRun<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Fd", ":FlutterDevices<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Fe", ":FlutterEmulators<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Fre", ":FlutterReload<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Frs", ":FlutterRestart<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Fq", ":FlutterQuit<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Fo", ":FlutterOutlineToggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>Fl", ":FlutterLspRestart<CR>", { noremap = true, silent = true })
