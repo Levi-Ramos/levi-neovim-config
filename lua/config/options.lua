1eba2fdb5bb47f771ceff0f16bc27e8ab33f1a86
cheerfulbeast <mlrmramos@usep.edu.ph>
2024-10-22 11:10:41 +0800

    chore: changed offscroll

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Add any additional options hereby
+
+local opt = vim.opt
+
+opt.scrolloff = 8
