175142d1c4def47b65cc4a223f8c43898baed183
Levi <mark.ramos@activesystems.ph>
2025-03-18 08:37:32 +0800

    Chore: removed dap configuration

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,16 +4,16 @@ return {
     local dap = require("dap")
 
     -- add more debug adapters here
-    dap.configurations.dart = {
-      {
-        type = "dart",
-        request = "launch",
-        name = "zeep launch flutter (linux)",
-        flutterSdkPath = "/home/apollo/flutter/bin/flutter",
-        dartSdkPath = "/home/apollo/flutter/bin/dart",
-        program = "${workspaceFolder}/lib/main.dart",
-        cwd = "${workspaceFolder}",
-      },
-    }
+    -- dap.configurations.dart = {
+    --   {
+    --     type = "dart",
+    --     request = "launch",
+    --     name = "Launch Flutter app",
+    --     flutterSdkPath = "C:\flutter\bin",
+    --     dartSdkPath = "C:\flutter\bin",
+    --     program = "${workspaceFolder}/lib/main.dart",
+    --     cwd = "${workspaceFolder}",
+    --   },
+    -- }
   end,
 }
