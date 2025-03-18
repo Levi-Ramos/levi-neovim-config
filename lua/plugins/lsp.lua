a5dfa7003d90d72d85a2e7b8e6bf799d7170193f
Levi <mark.ramos@activesystems.ph>
2025-03-18 08:38:00 +0800

    chore: removed lsp configuration for vue
    not using vue at the moment and this is useless, mason has vue

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,29 +6,4 @@ return {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
   },
-  config = function()
-    local lspconfig = require("lspconfig")
-    local mason_registry = require("mason-registry")
-
-    -- Retrieve the installation path for vue-language-server if it exists in Mason
-    local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
-      .. "/node_modules/@vue/language-server"
-
-    -- Configure tsserver with Vue TypeScript plugin
-    lspconfig.tsserver.setup({
-      init_options = {
-        plugins = {
-          {
-            name = "@vue/typescript-plugin",
-            location = vue_language_server_path,
-            languages = { "vue" },
-          },
-        },
-      },
-      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-    })
-
-    -- Configure volar (Vue Language Server)
-    lspconfig.volar.setup({})
-  end,
 }
