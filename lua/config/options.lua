449d6798fdc0e0764440e2775d9c580f4b1b6f43
Levi <mark.ramos@activesystems.ph>
2025-03-20 10:57:29 +0800

    chore: moved colorscheme to option

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options hereby
 
+-- vim.cmd.colorscheme("everforest")
 local opt = vim.opt
 
 opt.scrolloff = 8
