05613940a08d3212be3c4c7ce38bade79d6f16fa
Levi <mark.ramos@activesystems.ph>
2025-03-20 10:56:10 +0800

    chore: moved colorscheme to options

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -6,6 +6,5 @@ return {
     -- Optionally configure and load the colorscheme
     -- directly inside the plugin declaration.
     vim.g.everforest_enable_italic = true
-    vim.cmd.colorscheme("everforest")
   end,
 }
